d5aa104389666075398b2972cdbfc425c54b173c
Michael Arend <michael.arend@sartorius.com>
2023-06-22 11:07:08 +0200

    Library ausgelagert

--- Pio/Util/CFRFileUtil.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Client.Util
-{
-    static class CFRFileUtil
-    {
-        public static string[] GetAllCFRFiles(string directory) => Directory.GetFiles(directory).Where(x => x.ToUpper().EndsWith(".CFR")).ToArray();
-    }
-}
--- Program.cs
+++ Program.cs
@@ -1,88 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using Poker.Data;
+using PokerLibrary.Business;
 
-using Poker.Infrastructure.HistoryBuilder;
-using Poker.Infrastructure.Models;
-using Poker.Pio.Connection;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
+var gameHandler = new GameBusinessHandler();
+GameBusinessHandler.NewMessage += GameBusinessHandlerNewMessage;
 
-SolverConnection solver = new SolverConnection(@"C:\PioSolver\PioSOLVER2-pro.exe");
-
-
-AppDomain.CurrentDomain.ProcessExit += new EventHandler((i,p)=> CurrentDomainProcessExit(solver));
-
-static void CurrentDomainProcessExit( SolverConnection s)
+void GameBusinessHandlerNewMessage(object? sender, string e)
 {
-    s.Disconnect();
-    Console.WriteLine("Solver stopped.");
+    Console.WriteLine(e);
 }
 
-
-var baseCFRLocation = @"W:";
 var handAmount = Console.ReadLine();
-var historyBuilder = new HistoryBuilder();
-Stopwatch sw = new Stopwatch();
-sw.Start();
-for (var i = 0; i < int.Parse(handAmount); i++)
+if (Int16.TryParse(handAmount, out var hands))
 {
-   if(i%100 == 0)
-    {
-        Console.WriteLine($"running time: {sw.Elapsed.ToString()}");
-    }
-    var round = new Round(0.25m, 0.5m);
-    var preFlopRound = new PreflopRound();
-    historyBuilder.BuildHeader(round);
-    try
-    {
-        round = preFlopRound.PlayPreflop(round, historyBuilder);
-    }
-    catch (KeyNotFoundException e)
-    {
-        Console.WriteLine($"Betting Pattern not found: {e.Message}");
-        i--;
-        historyBuilder.Reset();
-        continue;
-    }
-    catch (Exception e)
-    {
-        i--;
-        continue;
-    }
-
-
-    try
-    {
-        switch (round.PlayersInHand.Count)
-        {
-            case 1:
-                Console.WriteLine($"Hand History {i}/{handAmount} created");
-                break;
-            case 2:
-
-                var postFlopRound = new PostflopRound(round, baseCFRLocation);
-                postFlopRound.PlayPostflop(historyBuilder, solver);
-                historyBuilder.SaveHistoryToFile("");
-                Console.WriteLine($"Hand History {i}/{handAmount} created");
-                break;
-
-            case > 2:
-                Console.WriteLine($"More than one Player:  {round.BettingPattern}");
-                i--;
-                break;
-
-        }
-    }
-    catch (Exception)
-    {
-        continue;
-    }
-    finally
-    {
-        historyBuilder.Reset();
-        
-    }
-
+    gameHandler.Play(CarrotsRanges.GetRanges(), hands, @"F:\", @"C:\PioSolver\PioSOLVER2-pro.exe", @"C:\PioSolver\hh.txt");
 }
+
+
+
+
 
 
 
